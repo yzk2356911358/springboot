05e4642a120cf5e9dc7d22c25c1e2d93440411a9
fk <2356911358@qq.com>
2018-12-03 21:49:23 +0800

    SpringBoot demo

--- src/main/java/com/yzk/springboot/SpringbootApplication.java
+++ src/main/java/com/yzk/springboot/SpringbootApplication.java
@@ -3,6 +3,7 @@ package com.yzk.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//测试
 @SpringBootApplication
 public class SpringbootApplication {
 
